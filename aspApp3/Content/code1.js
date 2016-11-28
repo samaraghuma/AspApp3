12a33e6c8dbde6821d2b147fd7b83bd1ec2b3b5c
raghuma sama <raghuma sama>
2016-11-28 14:40:19 -0500

    first commit

---
+++ aspApp3/Content/code1.js
@@ -0,0 +1,10 @@
+function getsum()
+{
+	
+	  var x = document.getElementById("firstnumber").value;
+	  x=parseInt(x);
+	    var y = document.getElementById("lastnumber").value;
+      y= parseInt(y);
+      document.getElementById("Sum").innerHTML= x+y
+
+}
---
+++ aspApp3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspApp3.Startup))]
+namespace aspApp3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
